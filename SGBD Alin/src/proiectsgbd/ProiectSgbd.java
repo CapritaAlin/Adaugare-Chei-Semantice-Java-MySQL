b947fb40441cc244dbf438afc6d0427039a52751
CapritaAlin <55480438+CapritaAlin@users.noreply.github.com>
2019-09-18 11:05:35 +0300

    Add files via upload

---
+++ SGBD Alin/src/proiectsgbd/ProiectSgbd.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proiectsgbd;
+
+/**
+ *
+ * @author Alin
+ */
+public class ProiectSgbd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PaginaPrincipala pp= new PaginaPrincipala();
+        pp.setVisible(true);
+        pp.setLocationRelativeTo(null);
+        
+    }
+    
+}
